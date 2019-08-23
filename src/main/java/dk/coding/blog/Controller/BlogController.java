e7c72cf4d2153b376a980260d1cabab710c10418
dk <113>
2019-08-23 21:29:02 +0800

    :art: 静态页面导入,可以预览效果

---
+++ src/main/java/dk/coding/blog/Controller/BlogController.java
@@ -0,0 +1,22 @@
+package dk.coding.blog.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 主页控制器.
+ */
+@Controller
+@RequestMapping("/blogs")
+public class BlogController {
+	
+	@GetMapping
+	public String listBlogs(@RequestParam(value="order",required=false,defaultValue="new") String order,
+			@RequestParam(value="keyword",required=false,defaultValue ="") String keyword) {
+		System.out.print("order:" +order + ";keyword:" +keyword );
+		return "redirect:/index?order="+order+"&keyword="+keyword;
+	}
+
+}
--- src/main/java/dk/coding/blog/Controller/MainController.java
+++ src/main/java/dk/coding/blog/Controller/MainController.java
@@ -1,68 +1,46 @@
-//package dk.coding.blog.Controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-///**
-// * 主页控制器.
-// */
-//@Controller
-//public class MainController {
-////	private static final Long ROLE_USER_AUTHORITY_ID = 2L;
-////	private final UserService userService;
-////	private final AuthorityService  authorityService;
-////
-////	@Autowired
-////	public MainController(UserService userService, AuthorityService authorityService) {
-////		this.userService = userService;
-////		this.authorityService = authorityService;
-////	}
-//
-//	@GetMapping("/")
-//	public String root() {
-//		return "redirect:index";
-//	}
-//
-//	@GetMapping("/index")
-//	public String index() {
-//		return "index";
-//	}
-//	/**
-//	 * 获取登录界面
-//	 */
-//	@GetMapping("/login")
-//	public String login() {
-//		return "login";
-//	}
-//
-//	@GetMapping("/login-error")
-//	public String loginError(Model model) {
-//		model.addAttribute("loginError", true);
-//		model.addAttribute("errorMsg", "登陆失败，账号或者密码错误！");
-//		return "login";
-//	}
-//
-//	@GetMapping("/register")
-//	public String register() {
-//		return "register";
-//	}
-//
-//	/**
-//	 * 注册用户
-//	 */
-////	@PostMapping("/register")
-////	public String registerUser(User user) {
-////		List<Authority> authorities = new ArrayList<>();
-////		authorities.add(authorityService.getAuthorityById(ROLE_USER_AUTHORITY_ID));
-////		user.setAuthorities(authorities);
-////		user.setEncodePassword(user.getPassword()); // 加密密码
-////		userService.saveUser(user);
-////		return "redirect:/login";
-////	}
-//
-//	@GetMapping("/search")
-//	public String search() {
-//		return "search";
-//	}
-//}
+package dk.coding.blog.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 主页控制器.
+ * 
+ * @since 1.0.0 2017年3月8日
+ * @author <a href="https://waylau.com">Way Lau</a> 
+ */
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/index";
+	}
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/login-error")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		model.addAttribute("errorMsg", "登陆失败，用户名或者密码错误！");
+		return "login";
+	}
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+	
+	@GetMapping("/search")
+	public String search() {
+		return "search";
+	}
+}
